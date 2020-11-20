fe5d0ab714a44a9e380281fe3754bdfd09508600
Filip Troníček <filip.tronicek@seznam.cz>
2020-11-20 23:34:13 +0100

    Format

--- js/index.js
+++ js/index.js
@@ -1,28 +1,23 @@
 var toConvert = $("#content").val();
 function generate(text) {
 
-  $("#qrcode").html("");
-  $("#qrcode").qrcode(text);
+    $("#qrcode").html("");
+    $("#qrcode").qrcode(text);
 }
 
 setInterval(() => {
-if(toConvert != $("#content").val()) {
-  toConvert = $("#content").val();
-  generate(toConvert);
-}
-if($("#content").val() == "") {
-  $("#qrcode").html("");
+    if (toConvert != $("#content").val()) {
+        toConvert = $("#content").val();
+        generate(toConvert);
+    }
+    if ($("#content").val() == "") {
+        $("#qrcode").html("");
 
-}
-  
+    }
+
 }, 10);
 var date = new Date();
 var month = date.getMonth();
 if (month == 11 || month == 0 || month == 1) {
-  var sf = new Snowflakes({
-    count: 50,
-    maxOpacity: 0.8,
-    maxSize: 20,
-    zIndex: 2
-  });
+    var sf = new Snowflakes({count: 50, maxOpacity: 0.8, maxSize: 20, zIndex: 2});
 }
